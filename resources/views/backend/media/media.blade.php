edb369ded8855cd0eee34611e84326660e76560a
KuntzeM <github@mathias-kuntze.de>
2016-11-07 17:29:02 +0100

    implement first step of file upload on client side

--- resources/views/backend/media/media.blade.php
+++ resources/views/backend/media/media.blade.php
@@ -14,16 +14,16 @@
 
     {!!Html::script('js/jquery.iframe-transport.js')  !!}
 
-    {!!Html::script('js/cors/jquery.postmessage-transport.js')  !!}
-    {!!Html::script('js/cors/jquery.xdr-transport.js')  !!}
+    <!-- {!!Html::script('js/cors/jquery.postmessage-transport.js')  !!}
+    {!!Html::script('js/cors/jquery.xdr-transport.js')  !!}-->
 
 
     <h3 class="item_details">{!! $title !!}</h3>
 
     @if($new)
-        {!! Form::open(['url' => $url . '/public/image', 'method' => 'post', 'enctype'=>"multipart/form-data"])  !!}
+        {!! Form::open(['url' => $url . '/auth/image', 'method' => 'post', 'enctype'=>"multipart/form-data"])  !!}
     @else
-        {!! Form::open(['url' => $url . '/public/image/'+ $media->media_id, 'method' => 'post'])  !!}
+        {!! Form::open(['url' => $url . '/public/image/' . $media->media_id, 'method' => 'post'])  !!}
     @endif
 
     <div class="form-group">
@@ -68,83 +68,35 @@
 
     {!! Form::close() !!}
     <script type="text/javascript">
-        $('#send').click(function (event) {
-            var request = new FormData();
-            request.append('token', '{!! $token !!}');
-            $.ajax({
-                url: '{!! $url . '/auth/image' !!}',
-                type: "POST",
-                data: request,
-                contentType: false,
-                cache: false,
-                processData: false,
-                xhr: function () {
-                    //upload Progress
-                    var xhr = $.ajaxSettings.xhr();
-                    if (xhr.upload) {
-                        xhr.upload.addEventListener('progress', function (event) {
-                            var percent = 0;
-                            var position = event.loaded || event.position;
-                            var total = event.total;
-                            if (event.lengthComputable) {
-                                percent = Math.ceil(position / total * 100);
-                            }
-                            //update progressbar
-                            $(".progress .progress-bar").css("width", +percent + "%");
-                            //$(progress_bar_id + " .status").text(percent +"%");
-                        }, true);
-                    }
-                    return xhr;
-                },
-                mimeType: "multipart/form-data",
-                crossDomain: true
-            }).done(function (res) { //
-                //$(my_form_id)[0].reset(); //reset form
-                //$(result_output).html(res); //output response from server
-                //submit_btn.val("Upload").prop( "disabled", false); //enable submit button once ajax is done
-            });
-        });
 
-
-        /*var request = new FormData();
-         request.append('a', 'upload');
-         request.append('token', '{!! $token !!}');
         $('#fileupload').fileupload({
          forceIframeTransport: true,
             type: 'POST',
          multipart: true,
-         forceIframeTransport: true,
-         data: request,
-            /*done: function (e, data) {
+            dataType: 'json',
+            processData: false,
+            contentType: false,
+            done: function (e, data) {
              console.log(e.message);
              console.log(data);
 
-         },
-         beforeSend: function (request)
-         {
-         request.setRequestHeader("Authority", '{!! $token !!}');
          },
             add: function (e, data) {
                 $('#send').click(function () {
                     data.submit();
                 });
          },
-             progressall: function (e, data) {
+            progressall: function (e, data) {
          var progress = parseInt(data.loaded / data.total * 100, 10);
-             $('.progress-bar').attr("aria-valuenow",progress);
-             $('.progress-bar').text(progress + '%');
-             $('.progress-bar').css('width',progress + '%');
+                $('.progress-bar').attr("aria-valuenow", progress)
+                        .text(progress + '%')
+                        .css('width', progress + '%');
          }
+        }).bind('fileuploadsubmit', function (e, data) {
+            var request = new FormData($('form')[0]);
+            data = request;
+
         });
-        $('#fileupload').bind('fileuploadsubmit', function (e, data) {
-            // The example input, doesn't have to be part of the upload form:
-         data.formData = '{!! $token !!}';
-            /*if (!data.formData.example) {
-             data.context.find('button').prop('disabled', false);
-             input.focus();
-             return false;
-         }
-         });*/
 
     </script>
 @stop
